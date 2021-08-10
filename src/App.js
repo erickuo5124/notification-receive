9468d94d4476223f74d6b95c96bc075405145f91
erickuo5124 <erickuo5124@gmail.com>
2021-08-11 00:51:33 +0800

    feat: first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import webpush from 'web-push'
 
 function App() {
+  const [status, setStatus] = useState('');
+  async function subscribe() {
+    if (!('serviceWorker' in navigator)) {
+      setStatus('no service worker!')
+      return
+    }
+    const vapidKeys = webpush.generateVAPIDKeys()
+    console.log(vapidKeys)
+    let sw = await navigator.serviceWorker.ready;
+    await sw.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: vapidKeys.publicKey
+    }).then(res => {
+      const serverData = JSON.stringify(res)
+      console.log(serverData);
+      sendData({
+        endpoint: serverData.endpoint,
+        p256dh: serverData.keys.p256dh,
+        auth: serverData.keys.auth,
+        private_key: vapidKeys.privateKey
+      })
+    })
+      .catch(err => console.log(err))
+
+  }
+  const sendData = async (data) => {
+    await fetch(`${process.env.REACT_APP_MAIN_HOST}/add`,
+      {
+        mode: 'no-cors',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      }
+    ).then(res => console.log(res))
+      .catch(err => console.log(err))
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +49,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        <button onClick={subscribe}>subscribe</button>
+        <p
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          {status}
+        </p>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -15,3 +15,9 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+  .then(reg => console.log('SW registered!', reg))
+  .catch(err => console.log('Error!', err));
+}
